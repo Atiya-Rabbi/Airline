ea7f6ede51cdaae60de56ad3eda2161f5b497a0e
Atiya Rabbi <31544068+Atiya-Rabbi@users.noreply.github.com>
2020-11-17 18:55:43 +0530

    delete record implemented

--- airline.py
+++ airline.py
@@ -37,21 +37,23 @@ except Exception as e:
 	print("Could not create table")
 	print(e)
 try:
-	command_handler.execute("CREATE TABLE Flight (flight_id INT PRIMARY KEY, code VARCHAR(3), FOREIGN KEY(code) REFERENCES Airport(code), destination VARCHAR(64), duration INT)")
+	command_handler.execute("CREATE TABLE Flight (flight_id INT PRIMARY KEY, code VARCHAR(3), FOREIGN KEY(code) REFERENCES Airport(code) ON DELETE CASCADE, destination VARCHAR(64), duration INT)")
 	print("Table Flight created")
 except Exception as e:
 	print("Could not create table")
 	print(e)
 try:
-	command_handler.execute("CREATE TABLE Passenger (id INT PRIMARY KEY AUTO_INCREMENT, first VARCHAR(64), last VARCHAR(64), flight_id INT, FOREIGN KEY(flight_id) REFERENCES Flight(flight_id))")
+	command_handler.execute("CREATE TABLE Passenger (id INT PRIMARY KEY AUTO_INCREMENT, first VARCHAR(64), last VARCHAR(64), flight_id INT, FOREIGN KEY(flight_id) REFERENCES Flight(flight_id) ON DELETE CASCADE)")
 	print("Table Passenger created")
 except Exception as e:
 	print("Could not create table")
 	print(e)
 
+#Keep on displaying options until user wants to exit
 while True:
 	print("Press 1 to add Airport \nPress 2 to add Flight \nPress 3 to add Passengers \nPress 4 to view details \nPress 5 to exit")
 	option = int(input("Option: "))
+	#insert into Airport table
 	if option == 1:
 		code = input("Enter Airport code: ")
 		city = input("Enter Airport city: ")
@@ -63,6 +65,7 @@ while True:
 		except Exception as e:
 			print("Could not add Airport")
 			print(e)
+	#insert into Flight table
 	elif option == 2:
 		flight_id = int(input("Enter Flight Id: "))
 		code = input("Enter Airport code: ")
@@ -76,6 +79,7 @@ while True:
 		except Exception as e:
 			print("Could not add Flight")
 			print(e)
+	#inser into passenger table
 	elif option == 3:
 		flight_id = int(input("Enter Flight Id of Passenger: "))
 		first = input("Enter first name: ")
@@ -89,8 +93,9 @@ while True:
 			print("Could not add Passenger")
 			print(e)
 	elif option == 4:
-		print("Press A for Airport details\nPress B for Flight details\nPress C for Passenger details")
+		print("Press A for Airport details\nPress B for Flight details\nPress C for Passenger details\nPress D to delete a record")
 		op = input("Option: ")
+		#display airport details
 		if op == 'A':
 			command_handler.execute("SELECT * FROM Airport")
 			tables = command_handler.fetchall()
@@ -106,13 +111,14 @@ while True:
 			else:
 				for table in tables:
 					print(table)
-		if op == 'B':
+		#display flight details
+		elif op == 'B':
 			command_handler.execute("SELECT * FROM Flight")
 			tables = command_handler.fetchall()
 			for table in tables:
 				print(table)
 			code = input("Enter code: ")
-			query = """SELECT A.city AS CITY, F.flight_id AS FLIGHTID, F.destination AS DESTINATION, F.duration AS DURATION FROM Airport A INNER JOIN Flight F ON (A.code = F.code) WHERE F.code = (%s)"""
+			query = """SELECT A.city, F.flight_id, F.destination, F.duration FROM Airport A INNER JOIN Flight F ON (A.code = F.code) WHERE F.code = (%s)"""
 			query_vals = (code,)
 			command_handler.execute(query,query_vals)
 			tables = command_handler.fetchall()
@@ -122,8 +128,9 @@ while True:
 				for table in tables:
 					print(table)
 				print('\n')
-		if op == "C":
-			command_handler.execute("SELECT flight_id, COUNT(id)FROM Passenger GROUP BY(flight_id)")
+		#display passenger details
+		elif op == "C":
+			command_handler.execute("SELECT P.flight_id, F.code, COUNT(P.id)FROM Passenger P INNER JOIN Flight F ON(F.flight_id=P.flight_id) GROUP BY(P.flight_id)")
 			tables = command_handler.fetchall()
 			for table in tables:
 				print(table)
@@ -138,6 +145,49 @@ while True:
 				for table in tables:
 					print(table)
 				print('\n')
+		elif op == "D":
+			print("Press 'a' to delete an Airport\nPress 'b' to delete a Flight\nPress 'c' to delete a Passenger")
+			pressed = input("Option: ")
+			#delete airport and associtaed flights
+			if pressed == 'a':
+				code = input("Enter Airport code: ")
+				print("NOTE: Deleting "+code+" will also delete associated flights and passengers")
+				cont = input("Do you want to continue?y/n ")
+				if cont == 'y':
+					query="""DELETE FROM Airport WHERE code = %s"""
+					query_vals=(code,)
+					command_handler.execute(query,query_vals)
+					db1.commit()
+				else:
+					print("Process Terminated")
+			#delete flight and associated passengers
+			elif pressed == 'b':
+				flight_id = int(input("Enter Flight Id: "))
+				print("NOTE: Deleting "+flight_id+" will also delete associated passengers")
+				cont = input("Do you want to continue?y/n ")
+				if cont == 'y':
+					query="""DELETE FROM Flight WHERE flight_id = %s"""
+					query_vals=(code,)
+					command_handler.execute(query,query_vals)
+					db1.commit()
+				else:
+					print("Process Terminated")
+			#delete passenger
+			elif pressed == 'c':
+				passenger_id = int(input("Enter Passenger Id: "))
+				cont = input("Do you want to continue?y/n ")
+				if cont == 'y':
+					query="""DELETE FROM Passenger WHERE id = %s"""
+					query_vals=(passenger_id,)
+					command_handler.execute(query,query_vals)
+					db1.commit()
+				else:
+					print("Process Terminated")			
+			else:
+				print("Wrong option")
+		else:
+			print("Wrong option")
+	#exit from while loop
 	elif option == 5:
 		break
 	else:
